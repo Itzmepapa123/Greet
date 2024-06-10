d27794544ff9b735c2ec3ac2d829bf2a82f7acbf
itzmeproman <atulpandey3030@gmail.com>
2024-06-10 19:43:29 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -15,7 +15,7 @@ from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL
 from helper_func import subscribed, encode, decode, get_messages
 from database.database import add_user, del_user, full_userbase, present_user
 async def delete_after_delay(message: Message, delay):
-    await asyncio.sleep(AUTO_DELETE_TIME)
+    await asyncio.sleep(600)
     await message.delete()
 
 
